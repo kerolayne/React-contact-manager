28d721f75541ce8c755157c3bbbb6392acaa4289
kerolayne <kerolaynesouza@outlook.com.br>
2025-07-14 22:02:51 +0100

    remove test keys add gitignore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDzW7UliP2NxI8jSWn_hQ1n90iGsOasvCI",
-  authDomain: "gestor-de-contatos.firebaseapp.com",
-  projectId: "gestor-de-contatos",
-  storageBucket: "gestor-de-contatos.firebasestorage.app",
-  messagingSenderId: "894966763634",
-  appId: "1:894966763634:web:0da06dab7ade03df1672eb",
-  measurementId: "G-2DX34N40NB"
+apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 console.log("A ser lido de firebaseConfig.js:", firebaseConfig);
